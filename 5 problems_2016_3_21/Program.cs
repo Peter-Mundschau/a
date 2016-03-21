b329045872440281a5c428803b2da033cb349704
Peter-Mundschau <peter.mundschau@outlook.com>
2016-03-21 11:36:45 -0500

    Start of Project

---
+++ 5 problems_2016_3_21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_problems_2016_3_21
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 5 problems_2016_3_21/ValueOfPi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_problems_2016_3_21
+{
+    class ValueOfPi
+    {
+    }
+}

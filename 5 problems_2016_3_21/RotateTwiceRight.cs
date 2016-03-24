fdd0c2895df5f663ca409b6ece8cd1a0ef3701ca
Peter-Mundschau <peter.mundschau@outlook.com>
2016-03-24 09:41:04 -0500

    Unable to convert string to Int

--- 5 problems_2016_3_21/RotateTwiceRight.cs
+++ 5 problems_2016_3_21/RotateTwiceRight.cs
@@ -7,24 +7,26 @@ using System.Threading.Tasks;
 namespace _5_problems_2016_3_21
 {
     public class RotateTwiceRight
-    {/*
+    {
         
 
-        RotatebyTwo()
+        public static void RotatebyTwo()
         {
             Console.WriteLine("Enter your Numbers Followed by a Coma;  eg;(1, 2, 3, )\n" );
             string userNumber = Console.ReadLine();
-            Console.ReadLine();
+            
 
-            List<string> listofNumbers = userNumber.Split(',').ToList();
+            List<string> listofNumbers = userNumber.Split(',').ToList<string>();
+            int numberListLength = listofNumbers.LongCount;
+            int numberListLength = listofNumbers.LongCount();
 
-            listofNumbers.Sort();
+            listofNumbers.Sort();   
 
-            return listofNumbers;   ?? NEED LENGTH OF LIST ??
+            return ;  
         }
 
         
-        */
+        
     
 
     }
